35d7d8c6521b8aaf5d9ace15c374f2664df03ef1
Abdul Samad <abdulsamad.laghari1@gmail.com>
2025-06-17 10:43:44 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const mongoUri =
-  process.env.DATABASE_URL || "mongodb://localhost:27017/ticketScraper";
+  process.env.DATABASE_URL;
 
 const connectDB = async () => {
   try {
